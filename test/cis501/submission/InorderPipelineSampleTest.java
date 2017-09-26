1a9e16a73b2984b2865e62665b123d86935061cb
Joe Devietti <joe@devietti.net>
2017-09-25 21:34:47 -0400

    stores always have an invalid destination reg now

--- test/cis501/submission/InorderPipelineSampleTest.java
+++ test/cis501/submission/InorderPipelineSampleTest.java
@@ -24,6 +24,7 @@ public class InorderPipelineSampleTest {
     private static List<Insn> wM_Dependency_StoreVal_Insns;
 
     private static Insn makeInsn(int dst, int src1, int src2, MemoryOp mop) {
+        if (MemoryOp.Store == mop) assert -1 == dst : dst;
         return new Insn(dst, src1, src2,
                 1, 4,
                 null, 0, null,
@@ -105,7 +106,7 @@ public class InorderPipelineSampleTest {
         List<Insn> insns = new LinkedList<>();
         final int COUNT = 10;
         for (int i = 0; i < COUNT; i++) {
-            insns.add(makeInsn(3, 1, 2, MemoryOp.Store)); // no load-use dependencies
+            insns.add(makeInsn(-1, 1, 2, MemoryOp.Store)); // no load-use dependencies
         }
         sim.run(insns);
         assertEquals(COUNT, sim.getInsns());
@@ -154,7 +155,7 @@ public class InorderPipelineSampleTest {
     public void testLoadUseStoreAddress() {
         List<Insn> insns = new LinkedList<>();
         insns.add(makeInsn(3, 1, 2, MemoryOp.Load));
-        insns.add(makeInsn(5, 4, 3, MemoryOp.Store)); // load to src reg 2 (store address), so we stall
+        insns.add(makeInsn(-1, 4, 3, MemoryOp.Store)); // load to src reg 2 (store address), so we stall
         sim.run(insns);
         assertEquals(2, sim.getInsns());
         // 123456789abc
@@ -168,7 +169,7 @@ public class InorderPipelineSampleTest {
     public void testLoadUseStoreValue() {
         List<Insn> insns = new LinkedList<>();
         insns.add(makeInsn(3, 1, 2, MemoryOp.Load));
-        insns.add(makeInsn(5, 3, 4, MemoryOp.Store)); // load to src reg 1 (store value), so no stall
+        insns.add(makeInsn(-1, 3, 4, MemoryOp.Store)); // load to src reg 1 (store value), so no stall
         sim.run(insns);
         assertEquals(2, sim.getInsns());
         // 123456789abcdef
