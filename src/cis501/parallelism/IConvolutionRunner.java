0e2326ef70dfeac2811f75d9cc33ba0d3d85ec54
Joe Devietti <joe@devietti.net>
2017-11-27 22:49:09 -0500

    files for HW7: Parallel Image Blur

---
+++ src/cis501/parallelism/IConvolutionRunner.java
@@ -0,0 +1,27 @@
+package cis501.parallelism;
+
+public interface IConvolutionRunner {
+
+    /**
+     * Perform a parallel convolution
+     *
+     * @param kernel   the matrix for the kernel to use
+     * @param src      the input image pixels
+     * @param startRow the starting row of the image
+     * @param numRows  the number of rows to process
+     * @param dst      the output image pixels
+     * @return ChannelValues for all pixels processed by the blur
+     */
+    ChannelValues parallelConvolution(float[][] kernel, Int2D src, int startRow, int numRows, Int2D dst);
+
+    /**
+     * Perform a sequential convolution
+     *
+     * @param kernel the matrix for the kernel to use
+     * @param src    the input image pixels
+     * @param dst    the output image pixels
+     * @return ChannelValues for all pixels processed by the blur
+     */
+    ChannelValues sequentialConvolution(float[][] kernel, Int2D src, Int2D dst);
+
+}
